941099c2a02bb4ee2887227eee7aafa8a4d2f5b1
Grzegorz Ullmann <gullmann@gmail.com>
2016-02-08 19:22:55 +0100

    add circle class

--- src/main/java/JpaLevel1/Startowa.java
+++ src/main/java/JpaLevel1/Startowa.java
@@ -1,7 +1,6 @@
 package JpaLevel1;
 
-import JpaLevel1.Domain.Person;
-import JpaLevel1.Domain.PersonRepo;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Random;
@@ -11,27 +10,9 @@ public class Startowa {
     public static void main(String[] args) {
 
 //        dodatkowy niepotrzebny komentarz
-        
+
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("jpa-spring-config.xml");
 
-        PersonRepo personRepo = ctx.getBean(PersonRepo.class);
-
-        int rnd = new Random().nextInt(100);
-        Person nowaOsoba = new Person();
-        nowaOsoba.setName("Gall Anonim " + rnd);
-        nowaOsoba.setAlias("Galius " + rnd);
-        System.out.println("przed persist: " + nowaOsoba);
-        personRepo.save(nowaOsoba);
-        System.out.println("po persist (patrz id): " + nowaOsoba);
-
-        for(Person p : personRepo.findAll()) {
-            System.out.println(p);
-        }
-
-        System.out.println("----------------");
-        for(Person p : personRepo.findByName("Kwalski Jan")) {
-            System.out.println(p);
-        }
 
 
         ctx.close();
